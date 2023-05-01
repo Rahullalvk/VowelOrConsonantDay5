198ee33938d3d24a055f4b219373be03439e4ff6
Rahulalvk <rahulal432@gmail.com>
2023-05-01 03:31:41 -0700

    Programe to check wheather the alphabet is Vowel or consonant

---
+++ AlphabetisVowelorConsonant/AlphabetisVowelorConsonant/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetisVowelorConsonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VowelOrConsonant.DisplayVowelOrConsonant();
+
+        }
+    }
+}
---
+++ AlphabetisVowelorConsonant/AlphabetisVowelorConsonant/VowelOrConsonent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetisVowelorConsonant
+{
+    internal class VowelOrConsonant
+    {
+        public static void DisplayVowelOrConsonant() 
+        {
+            char ch;
+
+            Console.WriteLine("Enter an alphabet: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("The entered alphabet is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("The entered alphabet is a consonant.");
+            }
+        }
+    }
+}
